3fbb20a440d9d6ac5ba1ae86bdf78dfac79234dd
Solaris <ovvo.system@gmail.com>
2023-10-26 16:13:52 -0300

    Connect to mongodb database

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,20 @@
-import express from 'express';
-import config from './server-config';
+import express from "express";
+import mongoose from "mongoose";
+import config from "./server-config";
 
 const app = express();
 const port = config.port;
+const db_url = config.db_url;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+mongoose
+    .connect(db_url)
+    .then(() => {
+        console.log("App connected to database");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
--- backend/server-config.ts
+++ backend/server-config.ts
@@ -1,5 +1,8 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+
 const config = {
     port: 3001,
+    db_url: process.env.DB_URL!,
 };
 
 export default config;
