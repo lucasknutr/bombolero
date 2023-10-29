d21f17fbc1affbfec8eea3169f4bf834e7b91e4a
Solaris <ovvo.system@gmail.com>
2023-10-29 15:19:37 -0300

    Add types to admin controller parameters

--- backend/controllers/adminController.ts
+++ backend/controllers/adminController.ts
@@ -1,20 +1,24 @@
+import { Request, Response } from "express";
+
 // @desc    Registers admin
-// route    POST /api/admin/register/
+// route    POST /api/admin/register
 // @access  Public
-function registerAdmin(req, res) {
+function registerAdmin(req: Request, res: Response) {
     res.status(200).json({ message: "Register Admin" });
 }
 
 // @desc    Authenticates admin
-// route    POST /api/admin/login/
+// route    POST /api/admin/login
 // @access  Public
-function loginAdmin(req, res) {
+function loginAdmin(req: Request, res: Response) {
     res.status(200).json({ message: "Login Admin" });
 }
 
 // @desc    Logs out admin
-// route    POST /api/admin/logout/
+// route    POST /api/admin/logout
 // @access  Public
-function logoutAdmin(req, res) {
+function logoutAdmin(req: Request, res: Response) {
     res.status(200).json({ message: "Logout Admin" });
 }
+
+export { registerAdmin, loginAdmin, logoutAdmin };
