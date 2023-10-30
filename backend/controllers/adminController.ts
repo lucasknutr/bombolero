802392e3d61b5c2c059182a31f80cd34a4ecbd0a
Solaris <ovvo.system@gmail.com>
2023-10-30 09:38:38 -0300

    Use express async handler

--- backend/controllers/adminController.ts
+++ backend/controllers/adminController.ts
@@ -1,24 +1,25 @@
 import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 
 // @desc    Registers admin
 // route    POST /api/admin/register
 // @access  Public
-function registerAdmin(req: Request, res: Response) {
+const registerAdmin = asyncHandler(async (req: Request, res: Response) => {
     res.status(200).json({ message: "Register Admin" });
-}
+});
 
 // @desc    Authenticates admin
 // route    POST /api/admin/login
 // @access  Public
-function loginAdmin(req: Request, res: Response) {
+const loginAdmin = asyncHandler(async (req: Request, res: Response) => {
     res.status(200).json({ message: "Login Admin" });
-}
+});
 
 // @desc    Logs out admin
 // route    POST /api/admin/logout
 // @access  Public
-function logoutAdmin(req: Request, res: Response) {
+const logoutAdmin = asyncHandler(async (req: Request, res: Response) => {
     res.status(200).json({ message: "Logout Admin" });
-}
+});
 
 export { registerAdmin, loginAdmin, logoutAdmin };
