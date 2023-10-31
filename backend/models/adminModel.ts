b6426ec35493d79f44c40c09c2da8126f6ba012a
Solaris <ovvo.system@gmail.com>
2023-10-31 09:58:38 -0300

    Hash password before saving model

--- backend/models/adminModel.ts
+++ backend/models/adminModel.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { genSalt, hash } from "bcrypt";
 
 const adminSchema = new mongoose.Schema(
     {
@@ -21,5 +22,14 @@ const adminSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await genSalt(10);
+    this.password = await hash(this.password, salt);
+});
 
 export const Admin = mongoose.model("Admin", adminSchema);
