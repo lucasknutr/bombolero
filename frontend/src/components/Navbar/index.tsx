9b065f2fe8bbd2330e7dab4b9f53effc8395c916
Lucas <lucasclaudinocc@gmail.com>
2023-11-04 12:06:40 -0300

    Added Admin Home page

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -36,7 +36,7 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="absolute" sx={{ backgroundColor: 'rgba(255,250,0,.2)' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import './style.css';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import ChecklistIcon from '@mui/icons-material/Checklist';
+
+
+const Home = () => {
+  return (
+    <>
+    <div className="background-options flex items-center justify-center h-screen">
+        <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                <div className="home-options">
+                    <CurrencyExchangeIcon className='icons' style={{ fontSize: 60 }}/>
+                    {/* <p>CAIXA</p> */}
+                </div>
+                <div className="home-options">
+                    <ChecklistIcon className='icons' style={{ fontSize: 60 }}/>
+                    {/* <p>ESTOQUE</p> */}
+                </div>
+                <div className="home-options" >
+                    ESTOQUE
+                </div>
+                <div className="home-options" >
+                    VER CARDÁPIO
+                </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home;
